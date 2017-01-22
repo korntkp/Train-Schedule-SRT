70d26fc48a97032958d1b566a2bababdde476031
shredktp <kornth_ebasicxp@hotmail.com>
2017-01-23 02:55:01 +0700

    Retrieve schedule from check time's SRT

--- app/src/main/java/io/github/shredktp/trainschedulesrt/MainActivity.java
+++ app/src/main/java/io/github/shredktp/trainschedulesrt/MainActivity.java
@@ -1,13 +1,98 @@
 package io.github.shredktp.trainschedulesrt;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+
+import io.github.shredktp.trainschedulesrt.srt_api.SrtApi;
+import io.github.shredktp.trainschedulesrt.srt_api.ToStringConverterFactory;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private static final String BASE_URL_SRT_CHECK_TIME = "http://www.railway.co.th/checktime/";
+
+    TextView tvHelloWorld;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupView();
+        setupApiCaller();
+//        setupWebViewFragment();
     }
+
+    private void setupApiCaller() {
+//        Gson gson = new GsonBuilder()
+//                .disableHtmlEscaping()
+//                .setLenient()
+//                .create();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL_SRT_CHECK_TIME)
+                .addConverterFactory(new ToStringConverterFactory())
+                .build();
+
+        SrtApi srtApi = retrofit.create(SrtApi.class);
+        Call<String> trainScheduleBodyCall = srtApi.getSchedule("", "");
+//        Call<String> trainScheduleBodyCall = srtApi.getSchedule("กรุงเทพ", "อยุธยา");
+
+        trainScheduleBodyCall.enqueue(new Callback<String>() {
+            @Override
+            public void onResponse(Call<String> call, Response<String> response) {
+                if (response.isSuccessful()) {
+                    Log.d(TAG, "onResponse Successful: " + response.body());
+                    Log.d(TAG, "onResponse Successful: " + response.message());
+                    Log.d(TAG, "onResponse Successful: " + response.toString());
+                    Log.d(TAG, "onResponse Successful: " + response.headers());
+                    Log.d(TAG, "onResponse Successful: " + response.errorBody());
+                    tvHelloWorld.setText(response.body());
+                } else {
+                    Log.d(TAG, "onResponse not successful: " + response.body());
+                    Log.d(TAG, "onResponse not successful: " + response.errorBody());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<String> call, Throwable t) {
+                Log.d(TAG, "onFailure: " + t.getMessage());
+                Log.d(TAG, "onFailure: " + t.getStackTrace());
+                Log.d(TAG, "onFailure: " + call.request().toString());
+                Log.d(TAG, "onFailure: " + call.request().body().contentType());
+                Log.d(TAG, "onFailure: " + call.request().body().toString());
+                tvHelloWorld.setText(t.getMessage());
+            }
+        });
+    }
+
+    private void setupView() {
+        tvHelloWorld = (TextView) findViewById(R.id.tv_hello_world);
+    }
+
+//    private void setupWebViewFragment() {
+//        ScheduleResultWebViewFragment scheduleResultWebViewFragment = ScheduleResultWebViewFragment.newInstance("", "");
+//        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+//        fragmentTransaction.replace(R.id.fragment_web_view_schedule, scheduleResultWebViewFragment);
+//        fragmentTransaction.commit();
+//    }
+
+
+//    if (response.isSuccessful()) {
+//        Log.d(TAG, "onResponse Successful: " + response.body());
+//        tvHelloWorld.setText(response.body());
+//    } else {
+//        Log.d(TAG, "onResponse not successful: " + response.body());
+//        Log.d(TAG, "onResponse not successful: " + response.errorBody());
+//    }
+
+
+//    Log.d(TAG, "onFailure: " + t.getMessage());
+//    tvHelloWorld.setText(t.getMessage());
 }
---
+++ app/src/main/java/io/github/shredktp/trainschedulesrt/srt_api/SrtApi.java
@@ -0,0 +1,16 @@
+package io.github.shredktp.trainschedulesrt.srt_api;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+/**
+ * Created by Korshreddern on 23-Jan-17.
+ */
+
+public interface SrtApi {
+    @FormUrlEncoded
+    @POST("checktime.asp")
+    Call<String> getSchedule(@Field("StationFirst") String stationFirst, @Field("StationLast") String stationLast);
+}
